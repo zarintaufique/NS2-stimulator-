ecfd0805565bf4c94fffb1458168d2efa285bd90
zarin <81378664+zarintaufique@users.noreply.github.com>
2022-01-23 15:18:52 +0500

    finish procedure added

--- code.tcl
+++ code.tcl
@@ -2,3 +2,11 @@ set ns [new Simulator]
 
 set nf [open out.nam w]
 $ns namtrace-all $nf
+
+proc finish {} {
+        global ns nf
+        $ns flush-trace
+        close $nf
+        exec nam out.nam &
+        exit 0
+}
